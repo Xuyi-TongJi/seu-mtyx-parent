c9bf615b785fde15ecfe02c4242bdfde92845295
Xuyi-TongJi <220210490@seu.edu.cn>
2023-06-18 00:44:23 +0800

    项目初始化

---
+++ service/service-acl/src/main/java/edu/seu/mtyx/ServiceAclApplication.java
@@ -0,0 +1,12 @@
+package edu.seu.mtyx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceAclApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceAclApplication.class, args);
+    }
+}
